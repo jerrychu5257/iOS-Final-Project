139777c4dec4b857e52e6ab24b61f25352399fac
JerryChu5257 <summer8341000@gmail.com>
2024-04-23 16:01:50 +1000

    add swift project

---
+++ iOS-assignment3/iOS-assignment3/iOS_assignment3App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_assignment3App.swift
+//  iOS-assignment3
+//
+//  Created by Chien Yuan Chu on 2024/4/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_assignment3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
